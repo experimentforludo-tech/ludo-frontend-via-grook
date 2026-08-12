58bb5e52a4c9c75811ee10df43266cf1c0981aea
experimentforludo-tech <experimentforludo@gmail.com>
2026-08-13 00:20:21 +0530

    Update haptics.js

--- src/utils/haptics.js
+++ src/utils/haptics.js
@@ -3,4 +3,4 @@ export function triggerVibration(type = 'light') {
   if (type === 'light') navigator.vibrate(30);
   else if (type === 'heavy') navigator.vibrate([70, 40, 70]);
   else if (type === 'win') navigator.vibrate([100, 60, 100, 60, 200]);
-}\n
+}
